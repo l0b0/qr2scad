2669a5698db9b8d6fca26ca372bf4bed44028efe
Victor Engmark <victor.engmark@gmail.com>
2013-07-20 21:51:45 +0200

    No need to include metadata in all files

--- qr2scad/qr2scad.py
+++ qr2scad/qr2scad.py
@@ -29,11 +29,6 @@ Bugs:
 Please email bug reports to victor dot engmark at gmail dot com.
 """
 
-__author__ = 'Victor Engmark'
-__email__ = 'victor.engmark@gmail.com'
-__copyright__ = 'Copyright (C) 2010 Victor Engmark'
-__license__ = 'GPLv3'
-
 from PIL import Image, ImageOps
 from signal import signal, SIGPIPE, SIG_DFL
 import sys
